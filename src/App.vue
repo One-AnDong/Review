ca6ce7572fac38ee3f8da1c7a3af4ae90994f5b4
One-AnDong <one-andong@foxmail.com>
2019-08-11 14:34:58 +0800

    简易的todolist

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,14 @@
 <template>
   <div id="app">
-    <input type="text"
-           v-model="msg">
-    <button @click="handleClick">增加</button>
-    <ul>
-      <todo-item v-for="item in todoList"
-                 :key="item"
-                 :item="item">
-        <template v-slot:item='data'>
-          <span :style="{fontSize:'18px',color:data.checked?'red':'blue'}">{{item}}</span>
-        </template>
-      </todo-item>
-    </ul>
+    <todo-list />
   </div>
 </template>
 <script>
-import TodoItem from './components/TodoItem'
+import TodoList from './components/TodoList'
 export default {
   name: 'app',
-  data: () => {
-    return {
-      msg: '',
-      todoList: []
-    }
-  },
-  methods: {
-    handleClick () {
-      this.todoList.push(this.msg)
-      this.msg = ''
-    }
-  },
   components: {
-    TodoItem
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="todolist">
+    <input type="text"
+           v-model="msg">
+    <button @click="handleClick">增加</button>
+    <ul>
+      <todo-item v-for="item in todoList"
+                 :key="item"
+                 :item="item">
+        <template v-slot:item='data'>
+          <span :style="{fontSize:'18px',color:data.checked?'red':'blue'}">{{item}}</span>
+        </template>
+      </todo-item>
+    </ul>
+  </div>
+</template>
+<script>
+import TodoItem from './TodoItem'
+export default {
+  data: () => {
+    return {
+      msg: '',
+      todoList: []
+    }
+  },
+  methods: {
+    handleClick () {
+      this.todoList.push(this.msg)
+      this.msg = ''
+    }
+  },
+  components: {
+    TodoItem
+  }
+}
+</script>
+
+<style>
+</style>
